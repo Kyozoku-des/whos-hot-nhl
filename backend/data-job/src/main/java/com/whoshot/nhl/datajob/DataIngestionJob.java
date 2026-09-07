f7eca7807951c4b9975581c71a5da8f4f84f80f7
Kyozoku-des <h.wilander@outlook.com>
2026-09-07 19:50:28 +0200

    refactor: remove unused import and enhance GameState enum handling

--- backend/data-job/src/main/java/com/whoshot/nhl/datajob/DataIngestionJob.java
+++ backend/data-job/src/main/java/com/whoshot/nhl/datajob/DataIngestionJob.java
@@ -3,7 +3,6 @@ package com.whoshot.nhl.datajob;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
--- backend/data-job/src/main/java/com/whoshot/nhl/datajob/dto/nhlapi/GameState.java
+++ backend/data-job/src/main/java/com/whoshot/nhl/datajob/dto/nhlapi/GameState.java
@@ -1,19 +1,26 @@
 package com.whoshot.nhl.datajob.dto.nhlapi;
 
+import java.util.Arrays;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
+import lombok.Getter;
+
+@Getter
 public enum GameState {
     FUT,
     LIVE,
-    FINAL;
+    FINAL,
+    UNKNOWN;
 
     @JsonCreator
     public static GameState fromString(String value) {
-        if (value == null) {
-            return null;
-        }
-        return GameState.valueOf(value.toUpperCase());
+        if (value == null) return UNKNOWN;
+        return Arrays.stream(values())
+                .filter(state -> state.name().equalsIgnoreCase(value))
+                .findFirst()
+                .orElse(UNKNOWN);
     }
 
     @JsonValue
